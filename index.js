e0a8508ff8281281d37baf724213eea1b06f5c87
uwu-osu <garrisonnichols7@gmail.com>
2020-05-27 17:14:20 -0400

    changes to !kick and !ban permissions

--- index.js
+++ index.js
@@ -89,15 +89,61 @@ client.on('message', message => {
     }
 });
 
+// Moderation is below
+
+
+client.on('message', message => {
+    // Ignore messages that aren't from a guild
+    if (!message.guild) return;
+
+    // If the message content starts with "!kick"
+    if (message.content.startsWith('!kick') && message.member.hasPermission('KICK_MEMBERS')) {
+        // Assuming we mention someone in the message, this will return the user
+        const user = message.mentions.users.first();
+        // If we have a user mentioned
+        if (user) {
+            // Now we get the member from the user
+            const member = message.guild.member(user);
+            // If the member is in the guild
+            if (member) {
+                /**
+                 * Kick the member
+                 * Make sure you run this on a member, not a user!
+                 * There are big differences between a user and a member
+                 */
+                member
+                    .kick('Optional reason that will display in the audit logs')
+                    .then(() => {
+                        // We let the message author know we were able to kick the person
+                        message.reply(`Successfully kicked ${user.tag}`);
+                    })
+                    .catch(err => {
+                        // An error happened
+                        // This is generally due to the bot not being able to kick the member,
+                        // either due to missing permissions or role hierarchy
+                        message.reply('I was unable to kick the member');
+                        // Log the error
+                        console.error(err);
+                    });
+            } else {
+                // The mentioned user isn't in this guild
+                message.reply("That user isn't in this guild!");
+            }
+            // Otherwise, if no user was mentioned
+        } else {
+            message.reply("You didn't mention the user to kick!");
+        }
+    }
+});
 // Log bot in using the token from https://discordapp.com/developers/applications/me
-client.login('your token here');
+client.login('NzE0MjQ5NDQwMTA5NTI3MDcx.XsuwDQ.nJeuGc5PJ7BCHsfweQZAnCcbwpY');
 
 client.on('message', message => {
     // Ignore messages that aren't from a guild
     if (!message.guild) return;
 
     // if the message content starts with "!ban"
-    if (message.content.startsWith('!ban')) {
+    if (message.content.startsWith('!ban') && message.member.hasPermission('BAN_MEMBERS')) {
         // Assuming we mention someone in the message, this will return the user
         const user = message.mentions.users.first();
         // If we have a user mentioned
